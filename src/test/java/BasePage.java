c930896ed5e19ec07db149d05fa0716ddc66a61d
zoricamarkovic <zorica_markovic@apple.com>
2022-06-13 16:08:18 +0200

    Updated the testng annotation.

--- src/test/java/BasePage.java
+++ src/test/java/BasePage.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 
 import java.time.Duration;
 import java.util.List;
@@ -14,7 +16,7 @@ public class BasePage {
 
     WebDriver driver;
 
-    @BeforeClass
+    @BeforeMethod
     public void getDriver (){
         driver = DriverWrapper.getWebDriver("chrome");
     }
